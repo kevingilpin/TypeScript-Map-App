9202c639f4c1b9fb3270744c2e03f7d53ee02500
Kevin Gilpin <kevin.gilpin@gmail.com>
2021-09-09 12:14:00 -0700

    Refactor Map class addMarker methods to single method

--- src/Map.ts
+++ src/Map.ts
@@ -1,5 +1,9 @@
-import { User } from './User';
-import { Company } from './Company';
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
 
 export class Map {
   private googleMap: google.maps.Map;
@@ -14,22 +18,12 @@ export class Map {
     });
   }
 
-  addUserMarker(user: User): void {
+  addMarker(marker: Mappable): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: user.location.lat,
-        lng: user.location.lng,
-      },
-    });
-  }
-
-  addCompanyMarker(company: Company): void {
-    new google.maps.Marker({
-      map: this.googleMap,
-      position: {
-        lat: company.location.lat,
-        lng: company.location.lng,
+        lat: marker.location.lat,
+        lng: marker.location.lng,
       },
     });
   }
