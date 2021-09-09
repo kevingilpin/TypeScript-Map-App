7471959747f24e7de49fe4356572b98efdbdeca7
Kevin Gilpin <kevin.gilpin@gmail.com>
2021-09-09 12:02:14 -0700

    Create addCompanyMarker method to Map class, and display company on map UI

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { Company } from './Company';
 import { Map } from './Map';
 
 const user = new User();
+const company = new Company();
 const map = new Map('map');
 
 map.addUserMarker(user);
+map.addCompanyMarker(company);
